cd6f6b4dbb23a7e40b34cb0b237882ee636c25e5
Pavel Kazlou <pavel.kazlou.dev@gmail.com>
2026-06-18 17:20:53 +0200

    feat: generate account proto

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 export * from './contracts/gen/auth';
+export {
+	Role,
+	GetAccountRequestDto,
+	GetAccountResponseDto,
+	ACCOUNT_V1_PACKAGE_NAME,
+	AccountServiceClient,
+	AccountServiceController,
+	AccountServiceControllerMethods,
+	ACCOUNT_SERVICE_NAME,
+} from './contracts/gen/account';
 export * from './contracts/proto/paths';
 export * from './exceptions/rpc';
