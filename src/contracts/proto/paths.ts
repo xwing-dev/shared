704e2d987dac0b3d4c7048992deeabda674719a2
Pavel Kazlou <pavel.kazlou.dev@gmail.com>
2026-06-18 17:05:37 +0200

    feat: add account proto

--- src/contracts/proto/paths.ts
+++ src/contracts/proto/paths.ts
@@ -2,4 +2,5 @@ import { join } from 'path';
 
 export const PROTO_PATHS = {
     AUTH: join(__dirname, '../../../proto/auth.proto'),
+    ACCOUNT: join(__dirname, '../../../proto/account.proto'),
 } as const;
