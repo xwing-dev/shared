c015ff042ffc23f924c5920780ea7fe484cecd9e
Pavel Kazlou <pavel.kazlou.dev@gmail.com>
2026-06-27 11:27:56 +0200

    add movie proto

--- src/contracts/proto/paths.ts
+++ src/contracts/proto/paths.ts
@@ -4,4 +4,6 @@ export const PROTO_PATHS = {
     AUTH: join(__dirname, '../../../proto/auth.proto'),
     ACCOUNT: join(__dirname, '../../../proto/account.proto'),
     USERS: join(__dirname, '../../../proto/users.proto'),
+    MOVIE: join(__dirname, '../../../proto/movie.proto'),
+    CATEGORY: join(__dirname, '../../../proto/category.proto'),
 } as const;
